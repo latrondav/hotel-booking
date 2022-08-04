91ac4593c3227b9f5d8d833e401064b9cc177685
latrondav <latrondav@gmail.com>
2022-08-04 15:41:44 +0300

    commit

--- baloosh/models.py
+++ baloosh/models.py
@@ -21,33 +21,3 @@ class Reservations(models.Model):
 
     def __str__(self):
         return str(self.name)
-
-    def calc(self, *args, **kwargs):
-        if self.roomtypes == "SSR":
-            self.amounts = self.noofrooms * 100
-        elif self.roomtypes == "DSR":
-            self.amounts = self.noofrooms * 200
-        elif self.roomtypes == "TSR":
-            self.amounts = self.noofrooms * 300
-        elif self.roomtypes == "QSR":
-            self.amounts = self.noofrooms * 400
-        elif self.roomtypes == "QuSR":
-            self.amounts = self.noofrooms * 500
-        elif self.roomtypes == "KSR":
-            self.amounts = self.noofrooms * 600
-        elif self.roomtypes == "TwSR":
-            self.amounts = self.noofrooms * 700
-        elif self.roomtypes == "StSR":
-            self.amounts = self.noofrooms * 800
-        elif self.roomtypes == "SDR":
-            self.amounts = self.noofrooms * 900
-        elif self.roomtypes == "DDR":
-            self.amounts = self.noofrooms * 1000
-        elif self.roomtypes == "HS":
-            self.amounts = self.noofrooms * 120
-        elif self.roomtypes == "ES":
-            self.amounts = self.noofrooms * 2000
-        else:
-            self.amounts = 0
-        
-        return super.calc(self, *args, **kwargs)
